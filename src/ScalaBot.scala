6640a0790269d199ca003a52ca9b04f5b5c3bf69
Daniel Smith <jellymann@gmail.com>
2013-07-30 23:51:09 +0200

    Initial Commit

---
+++ src/ScalaBot.scala
@@ -0,0 +1,61 @@
+import afk.bot.london.LargeTank
+import java.util.Random
+import com.hackoeur.jglm.support.FastMath._
+
+class ScalaBot extends LargeTank {
+  var movement: Int = 0
+  var rotation: Int = 0
+  var action: () => Unit = move
+  var direction: () => Unit = turnClockwise
+  var rand: Random = new Random
+  
+  override def run() {
+    if (events hitWall) {
+      movement = 200
+      rotation = 180
+      action = turn
+    }
+    
+    var vis = events getVisibleBots;
+    
+    if (vis.length > 0) {
+      var diff = abs(vis(0))
+
+      if (diff < 1)
+      {
+          attack();
+      } else
+      {
+          if (vis(0) < 0)
+          {
+              turnAntiClockwise();
+          }
+          else
+          {
+              turnClockwise();
+          }
+      }
+    }
+  }
+  
+  def turn() {
+        if (rotation > 0) {
+            direction()
+            rotation = rotation-1
+        } else {
+            rotation = rand nextInt 180
+            direction = if (rand nextBoolean) turnClockwise else turnAntiClockwise
+            action = move;
+        }
+    }
+
+    def move() {
+        if (movement > 0) {
+            moveForward()
+            movement = movement-1
+        } else {
+            movement = rand nextInt 800
+            action = turn
+        }
+    }
+}

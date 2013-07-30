a4304a362e8a09c6fa07e72f735298e64052b6cd
Daniel Smith <jellymann@gmail.com>
2013-07-31 00:32:37 +0200

    Fixed bot not moving by adding call to action function.

--- src/ScalaBot.scala
+++ src/ScalaBot.scala
@@ -10,52 +10,52 @@ class ScalaBot extends LargeTank {
   var rand: Random = new Random
   
   override def run() {
-    if (events hitWall) {
+    if (events.hitWall) {
       movement = 200
       rotation = 180
       action = turn
     }
     
-    var vis = events getVisibleBots;
+    var vis = events.getVisibleBots;
     
     if (vis.length > 0) {
       var diff = abs(vis(0))
 
       if (diff < 1)
       {
-          attack();
+        attack();
       } else
       {
-          if (vis(0) < 0)
-          {
-              turnAntiClockwise();
-          }
-          else
-          {
-              turnClockwise();
-          }
+        if (vis(0) < 0)
+        {
+          turnAntiClockwise();
+        }
+        else
+        {
+          turnClockwise();
+        }
       }
-    }
+    } else action()
   }
   
   def turn() {
-        if (rotation > 0) {
-            direction()
-            rotation = rotation-1
-        } else {
-            rotation = rand nextInt 180
-            direction = if (rand nextBoolean) turnClockwise else turnAntiClockwise
-            action = move;
-        }
+    if (rotation > 0) {
+      direction()
+      rotation = rotation-1
+    } else {
+      rotation = rand nextInt 180
+      direction = if (rand.nextBoolean) turnClockwise else turnAntiClockwise
+      action = move;
     }
+  }
 
-    def move() {
-        if (movement > 0) {
-            moveForward()
-            movement = movement-1
-        } else {
-            movement = rand nextInt 800
-            action = turn
-        }
+  def move() {
+    if (movement > 0) {
+      moveForward()
+      movement = movement-1
+    } else {
+      movement = rand nextInt 800
+      action = turn
     }
+  }
 }
